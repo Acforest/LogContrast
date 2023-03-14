10bd5afebeff454d5d4fa3c12cf653cd8c2a627d
huangxin <vip923202114@vip.qq.com>
2023-03-14 18:23:03 +0800

    change `auto.sh` for exp1

--- scripts/exp1/auto.sh
+++ scripts/exp1/auto.sh
@@ -1,3 +1,4 @@
+sh ./scripts/exp1/hdfs_cl_bert_both_sup0.2_epoch20.sh
 sh ./scripts/exp1/hdfs_cl_bert_semantics_sup0.2_epoch20.sh
 sh ./scripts/exp1/hdfs_cl_bert_logkey_sup0.2_epoch20.sh
 sh ./scripts/exp1/bgl_cl_bert_both_sup0.2_epoch20.sh
