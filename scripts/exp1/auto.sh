484937ddedea93c6f17134405b59eddcc315a052
huangxin <vip923202114@vip.qq.com>
2023-03-14 18:10:50 +0800

    change path of `auto.sh` for exp1

--- scripts/exp1/auto.sh
+++ scripts/exp1/auto.sh
@@ -1,5 +1,5 @@
-python ./scripts/exp1/scripts/exp1/hdfs_cl_bert_semantics_sup0.2_epoch20.sh
-python ./scripts/exp1/scripts/exp1/hdfs_cl_bert_logkey_sup0.2_epoch20.sh
-python ./scripts/exp1/scripts/exp1/bgl_cl_bert_both_sup0.2_epoch20.sh
-python ./scripts/exp1/scripts/exp1/bgl_cl_bert_semantics_sup0.2_epoch20.sh
-python ./scripts/exp1/scripts/exp1/bgl_cl_bert_logkey_sup0.2_epoch20.sh
+python ./scripts/exp1/hdfs_cl_bert_semantics_sup0.2_epoch20.sh
+python ./scripts/exp1/hdfs_cl_bert_logkey_sup0.2_epoch20.sh
+python ./scripts/exp1/bgl_cl_bert_both_sup0.2_epoch20.sh
+python ./scripts/exp1/bgl_cl_bert_semantics_sup0.2_epoch20.sh
+python ./scripts/exp1/bgl_cl_bert_logkey_sup0.2_epoch20.sh
